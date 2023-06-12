5985a168ca9a3c371fdf86485613e6093605ad0a
unknown <vanducduy111000222@gmail.com>
2023-06-12 14:46:17 +0700

    commit 6

--- core/views.py
+++ core/views.py
@@ -87,7 +87,6 @@ def logout_view(request):
     return render(request, 'Signin.html')
 
 
-@login_required(login_url='Signin')
 def auction_view(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
